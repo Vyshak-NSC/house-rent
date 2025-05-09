a481d6153c38edf6a5a4611f6a55b26498ad566b
Vyshak-NSC <vyshaknsc02@gmail.com>
2025-05-09 23:36:14 +0530

    register page and system integrated

---
+++ frontend/house-rental/app/api/api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://127.0.0.1:8000/api/users/';
+
+export const registerUser = async (formData) => {
+    const response = await axios.post(`${API_BASE_URL}register/`, formData, {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    return response.data;
+};
